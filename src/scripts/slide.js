4c775492a4085f818ca5a5333d5b381a838c3770
henriquebazi <henriquebazi96@gmail.com>
2021-08-27 08:35:55 -0300

    touch events added

--- src/scripts/slide.js
+++ src/scripts/slide.js
@@ -20,26 +20,38 @@ export default class Slide {
   }
 
   onStart(event) {
-    event.preventDefault()
+    let moveType 
+    if (event.type === 'mousedown') {
+      event.preventDefault()
 
-    this.distances.startX = event.clientX
-    this.content.addEventListener('mousemove', this.onMove)
+      moveType = 'mousemove'
+      this.distances.startX = event.clientX
+    } else {
+      moveType = 'touchmove'
+      this.distances.startX = event.changedTouches[0].clientX
+    }
+
+    this.content.addEventListener(moveType, this.onMove)
   }
 
   onMove(event) {
-    const finalPosition = this.updatePostion(event.clientX)
+    const pointerPostion = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX
+    const finalPosition = this.updatePostion(pointerPostion)
 
     this.moveSlide(finalPosition)
   }
 
-  onEnd() {
-    this.content.removeEventListener('mousemove', this.onMove)
+  onEnd(event) {
+    const moveType = (event.type === 'mouseup') ? 'mousemove' : 'touchmove'
+    this.content.removeEventListener(moveType, this.onMove)
     this.distances.finalPosition = this.distances.movePosition
   }
 
   addSlideEvents() {
     this.content.addEventListener('mousedown', this.onStart)
+    this.content.addEventListener('touchstart', this.onStart)
     this.content.addEventListener('mouseup', this.onEnd)
+    this.content.addEventListener('touchend', this.onEnd)
   }
 
   bindEvents(){
