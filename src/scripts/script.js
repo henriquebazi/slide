c9680d74578af0a6017d72568be48925b8c0289f
henriquebazi <henriquebazi96@gmail.com>
2021-08-30 08:49:34 -0300

    custom controls slide

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,4 +3,4 @@ import { SlideNav } from './slide.js'
 const slide = new SlideNav('.content', '.slide')
 slide.init()
 slide.addArrow('.prev', '.next')
-slide.addControl()
+slide.addControl('.custom-controls')
